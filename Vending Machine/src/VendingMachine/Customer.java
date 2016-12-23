1568a8ed6ee1e8506d7cc33c2c69ce965c20d8d6
청운 <@192.168.0.46>
2016-12-23 09:04:39 +0900

    Final commit Chungwoon

--- Vending Machine/src/VendingMachine/Customer.java
+++ Vending Machine/src/VendingMachine/Customer.java
@@ -7,12 +7,24 @@ public class Customer {
 
 	String name;
 	String beverage;
-	double paid = 0;
-	double balance = 0;
+	double paid;
+	double balance;
+	double change;
 
-	public Customer(String name, double balance) {
+	public Customer(String name) {
 		this.name = name;
-		this.balance = balance;
+	}
+
+	public void balance() {
+		for (;;) {
+			System.out.printf("How much money do I have?\n$");
+			this.balance = input.nextDouble();
+			if (this.balance >= 0.5) {
+				break;
+			} else {
+				System.out.println("I'm afraid I don't have enough money to buy anything.");
+			}
+		}
 	}
 
 	public void observe(Customer customer, Machine machine) {
@@ -21,18 +33,50 @@ public class Customer {
 	}
 
 	public void select(Customer customer, Machine machine) {
+		System.out.println("What would you like to drink?");
+		System.out.printf("I have $%.2f.\n", this.balance);
+		machine.check(this, machine, beverage, paid, balance, change);
+	}
+
+	public void get(Customer customer, Machine machine, String beverage, double paid, double balance, double change) {
+		if (this.change > 0) {
+			System.out.printf("Here is your %s and your change $%.2f.\n", beverage, this.change);
+		} else {
+			System.out.printf("Here is your %s.\n", beverage);
+		}
+		System.out.printf("I have $%.2f left.\n", this.balance);
+		machine.checkIf(this, machine, this.balance);
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String again;
+
+		System.out.println("What is my name?");
+		Customer myCustomer = new Customer(sc.nextLine());
+		Machine myMachine = new Machine();
+
+		myCustomer.balance();
 		for (;;) {
-			System.out.println("What would you like to drink?");
-			beverage = input.nextLine();
-			System.out.printf("Please insert money. You have $%2.f\n", this.balance);
-			paid = input.nextDouble();
-			if (paid > balance) {
-				System.out.println("Please check your balance again.");
-			} else {
-				balance = balance - paid;
+			myCustomer.observe(myCustomer, myMachine);
+			if (myCustomer.balance < 0.5) {
+				System.out.println("I don't have enough money to get anything.");
+				break;
+			}
+			for (;;) {
+				System.out.println("Would you like to get another one?\nYes? or No?");
+				again = sc.nextLine();
+				if (again.equals("Yes") || again.equals("No")) {
+					break;
+				} else {
+					System.out.println("Error. Invalid input.");
+				}
+			}
+			if (again.equals("No")) {
+				System.out.println(myCustomer.name + ": I guess I'm good.");
 				break;
 			}
 		}
-		machine.check(this, machine, beverage, paid);
+
 	}
 }
--- Vending Machine/src/VendingMachine/Machine.java
+++ Vending Machine/src/VendingMachine/Machine.java
@@ -1,5 +1,7 @@
 package VendingMachine;
 
+import java.util.Scanner;
+
 public class Machine {
 	public Machine() {
 	}
@@ -9,16 +11,92 @@ public class Machine {
 		customer.select(customer, this);
 	}
 
-	public void check(Customer customer, Machine machine, String beverage, double paid) {
-		if (beverage.equals("Coke")) {
-			if (paid >= 1.20) {
-				System.out.println("Here is your coke.");
-			} else {
-				System.out.println("Please check if you put enough amount of money.");
+	public void check(Customer customer, Machine machine, String beverage, double paid, double balance, double change) {
+		Scanner input = new Scanner(System.in);
+
+		for (;;) {
+			for (;;) {
+				beverage = input.nextLine();
+				if (beverage.equals("Coke") && customer.balance >= 1.2) {
+					break;
+				} else if (beverage.equals("Sprite") && customer.balance >= 1.2) {
+					break;
+				} else if (beverage.equals("Pepsi") && customer.balance >= 1) {
+					break;
+				} else if (beverage.equals("Let's be") && customer.balance >= 0.5) {
+					break;
+				} else {
+					System.out.println("Error. Invalid input. Please try again.");
+				}
 			}
-		} else if (beverage.equals("Sprite")) {
-			
+			if (beverage.equals("Coke")) {
+				for (;;) {
+					System.out.printf("Please insert money.\n$");
+					customer.paid = input.nextDouble();
+					if (customer.paid >= 1.2 && customer.paid <= customer.balance) {
+						break;
+					} else if (customer.paid < 1.2 && customer.paid <= customer.balance) {
+						System.out.println("Please check if you put enough amount of money.");
+					} else {
+						System.out.println("Please check out your balance again.");
+					}
+				}
+				customer.change = customer.paid - 1.20;
+				customer.balance = customer.balance - customer.paid + customer.change;
+				break;
+			} else if (beverage.equals("Sprite")) {
+				for (;;) {
+					System.out.printf("Please insert money.\n$");
+					customer.paid = input.nextDouble();
+					if (customer.paid >= 1.2 && customer.paid <= customer.balance) {
+						break;
+					} else if (customer.paid < 1.2 && customer.paid <= customer.balance) {
+						System.out.println("Please check if you put enough amount of money.");
+					} else {
+						System.out.println("Please check out your balance again.");
+					}
+				}
+				customer.change = customer.paid - 1.20;
+				customer.balance = customer.balance - customer.paid + customer.change;
+				break;
+			} else if (beverage.equals("Pepsi")) {
+				for (;;) {
+					System.out.printf("Please insert money.\n$");
+					customer.paid = input.nextDouble();
+					if (customer.paid >= 1 && customer.paid <= customer.balance) {
+						break;
+					} else if (customer.paid < 1 && customer.paid <= customer.balance) {
+						System.out.println("Please check if you put enough amount of money.");
+					} else {
+						System.out.println("Please check out your balance again.");
+					}
+				}
+				customer.change = customer.paid - 1;
+				customer.balance = customer.balance - customer.paid + customer.change;
+				break;
+			} else if (beverage.equals("Let's be")) {
+				for (;;) {
+					System.out.printf("Please insert money.\n$");
+					customer.paid = input.nextDouble();
+					if (customer.paid >= 0.5 && customer.paid <= customer.balance) {
+						break;
+					} else if (customer.paid < 0.5 && customer.paid <= customer.balance) {
+						System.out.println("Please check if you put enough amount of money.");
+					} else {
+						System.out.println("Please check out your balance again.");
+					}
+				}
+				customer.change = customer.paid - 0.5;
+				customer.balance = customer.balance - customer.paid + customer.change;
+				break;
+			} else {
+				System.out.println("Error. Invalid input. Please try again.");
+			}
 		}
+		customer.get(customer, this, beverage, paid, balance, change);
 	}
 
+	public double checkIf(Customer customer, Machine machine, double balance) {
+		return customer.balance;
+	}
 }
